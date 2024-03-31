a7017db95a685ad6c281ef6dbb79cb6650387f84
sejongPark <lifedesigner88@gmail.com>
2024-03-31 19:47:53 +0900

    nuxtLink 입니다

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav style="display: flex; gap: 10px">
+      <a href="/">SSR Home</a>
+      <NuxtLink to="/">CSR Home</NuxtLink>
+      <a href="/movies">SSR Moveies</a>
+      <NuxtLink to ="/movies">CSR Moveies</NuxtLink>
+    </nav>
+    <NuxtPage/>
   </div>
 </template>
