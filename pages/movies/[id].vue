8eee1c81c3b289943ebb18ec0777bc396fafb611
sejongPark <lifedesigner88@gmail.com>
2024-03-31 22:34:38 +0900

    axios 내장되어 있따 굿굿

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1>{{ $route.params.id }}</h1>
-    http://localhost:3000/movies/123
+<!--    http://localhost:3000/movies/123-->
   </div>
 </template>
--- pages/teams/[teamSlug]/users/[userId].vue
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -1,11 +1,16 @@
-
-
-
+<script setup>
+const route = useRoute();
+const message = computed(() =>
+    `I'm on team ${route.params.teamSlug}
+    and my user id is ${route.params.userId}`
+)
+</script>
 
 <template>
 <p>
-  {{ $route.params.teamSlug }}
-  {{ $route.params.userId }}
+<!--  {{ $route.params.teamSlug }}<br>-->
+<!--  {{ $route.params.userId }}<br>-->
+  {{ message }}<br>
 </p>
 </template>
 
